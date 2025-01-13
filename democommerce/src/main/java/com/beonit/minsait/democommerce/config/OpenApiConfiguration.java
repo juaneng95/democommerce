ae59adea6bfff44c973e33595181fa987b24b216
juaneng95 <juaneng95@gmail.com>
2025-01-13 22:51:41 +0100

    Feat: Open api spec.

---
+++ democommerce/src/main/java/com/beonit/minsait/democommerce/config/OpenApiConfiguration.java
@@ -0,0 +1,14 @@
+package com.beonit.minsait.democommerce.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(
+    info =
+        @Info(
+            title = "Beonit-Minsait ecommerce artifact",
+            description = "Beonit-Minsait ecommerce API",
+            version = "v1"))
+@Configuration
+public class OpenApiConfiguration {}
