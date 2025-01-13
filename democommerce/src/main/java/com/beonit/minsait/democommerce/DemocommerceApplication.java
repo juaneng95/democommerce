c20d8585fe6b2a744177070908e5f74dff276fa9
juaneng95 <juaneng95@gmail.com>
2025-01-13 21:37:56 +0100

    Initial commit

---
+++ democommerce/src/main/java/com/beonit/minsait/democommerce/DemocommerceApplication.java
@@ -0,0 +1,13 @@
+package com.beonit.minsait.democommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemocommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemocommerceApplication.class, args);
+	}
+
+}
---
+++ democommerce/src/test/java/com/beonit/minsait/democommerce/DemocommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beonit.minsait.democommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemocommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
